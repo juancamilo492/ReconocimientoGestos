768dab82026d8fbdeec77529ba21d0a368b76e91
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-09-05 12:34:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import cv2
 import numpy as np
-#from PIL import Image
-from PIL import Image as Image, ImageOps as ImagOps
+from PIL import Image
 from keras.models import load_model
 
 import platform
@@ -14,79 +12,72 @@ model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes")
-#st.write("Versión de Python:", platform.python_version())
+
+# Mostrar una imagen inicial
 image = Image.open('OIG5.jpg')
 st.image(image, width=350)
+
 with st.sidebar:
-    st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
+    st.subheader("Usa un modelo entrenado en Teachable Machine para identificar imágenes")
     
-cam_ = st.checkbox("Usar Cámara")
-if cam_ :
-   img_file_buffer = st.camera_input("Toma una Foto")
-else :
-   img_file_buffer = None
+    # Opción para usar la cámara
+    cam_ = st.checkbox("Usar Cámara")
+    # Opción para subir una imagen desde el archivo
+    upload_ = st.file_uploader("Sube una imagen", type=["jpg", "jpeg", "png"])
 
-uploaded_image = st.file_uploader("Cargar Imagen:", type=["png", "jpg"])
-
-if uploaded_image is not None:
-
-    # To read image file buffer with OpenCV:
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-    img = Image.open(uploaded_image)
-
-    newsize = (224, 224)
-    img = img.resize(newsize)
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
-
-    # Normalize the image
-    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Palma, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('Ok, con Probabilidad: '+str( prediction[0][1]))
-    if prediction[0][2]>0.5:
-      st.header('JCBG, con Probabilidad: '+str( prediction[0][2]))
-    if prediction[0][3]>0.5:
-      st.header('Vacío, con Probabilidad: '+str( prediction[0][3]))
-    
-    
-
-
+if cam_:
+    img_file_buffer = st.camera_input("Toma una Foto")
+else:
+    img_file_buffer = None
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-   #To read image file buffer as a PIL Image:
+    # Lee el buffer de la imagen como una imagen PIL
     img = Image.open(img_file_buffer)
 
     newsize = (224, 224)
     img = img.resize(newsize)
-    # To convert PIL Image to numpy array:
+    # Convierte la imagen PIL a un array numpy
     img_array = np.array(img)
 
-    # Normalize the image
+    # Normaliza la imagen
     normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
+    # Carga la imagen en el array
     data[0] = normalized_image_array
 
-    # run the inference
+    # Ejecuta la inferencia
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Palma, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('Ok, con Probabilidad: '+str( prediction[0][1]))
-    if prediction[0][2]>0.5:
-      st.header('JCBG, con Probabilidad: '+str( prediction[0][2]))
-    if prediction[0][3]>0.5:
-      st.header('Vacío, con Probabilidad: '+str( prediction[0][3]))
+    if prediction[0][0] > 0.5:
+        st.header('Palma, con Probabilidad: ' + str(prediction[0][0]))
+    if prediction[0][1] > 0.5:
+        st.header('Ok, con Probabilidad: ' + str(prediction[0][1]))
+    if prediction[0][2] > 0.5:
+        st.header('JCBG, con Probabilidad: ' + str(prediction[0][2]))
+    if prediction[0][3] > 0.5:
+        st.header('Vacío, con Probabilidad: ' + str(prediction[0][3]))
 
+if upload_ is not None:
+    # Lee el archivo subido como una imagen PIL
+    img = Image.open(upload_)
 
+    newsize = (224, 224)
+    img = img.resize(newsize)
+    # Convierte la imagen PIL a un array numpy
+    img_array = np.array(img)
+
+    # Normaliza la imagen
+    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+    # Carga la imagen en el array
+    data[0] = normalized_image_array
+
+    # Ejecuta la inferencia
+    prediction = model.predict(data)
+    print(prediction)
+    if prediction[0][0] > 0.5:
+        st.header('Palma, con Probabilidad: ' + str(prediction[0][0]))
+    if prediction[0][1] > 0.5:
+        st.header('Ok, con Probabilidad: ' + str(prediction[0][1]))
+    if prediction[0][2] > 0.5:
+        st.header('JCBG, con Probabilidad: ' + str(prediction[0][2]))
+    if prediction[0][3] > 0.5:
+        st.header('Vacío, con Probabilidad: ' + str(prediction[0][3]))
