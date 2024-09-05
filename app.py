c0886978b3325746d267eeaec1c3cda6c85872da
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-09-05 12:50:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -57,31 +57,8 @@ if img_file_buffer is not None:
         st.header('Vacío, con Probabilidad: ' + str(prediction[0][3]))
 
 if upload_ is not None:
-    # Lee el archivo subido como una imagen PIL
-    img = Image.open(upload_)
-
-    newsize = (224, 224)
-    img = img.resize(newsize)
-    # Convierte la imagen PIL a un array numpy
-    img_array = np.array(img)
-    
-    # Normaliza la imagen
-    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    # Carga la imagen en el array
+    uploaded_file=bg_image
     st.image(uploaded_file, caption='Imagen cargada.', use_column_width=True)
-    st.header("xd")
-    data[0] = normalized_image_array
 
-    # Ejecuta la inferencia
-    prediction = model.predict(data)
-    print(prediction)
-    if prediction[0][0] > 0.5:
-        st.header('Palma, con Probabilidad: ' + str(prediction[0][0]))
-    if prediction[0][1] > 0.5:
-        st.header('Ok, con Probabilidad: ' + str(prediction[0][1]))
-    if prediction[0][2] > 0.5:
-        st.header('JCBG, con Probabilidad: ' + str(prediction[0][2]))
-    if prediction[0][3] > 0.5:
-        st.header('Vacío, con Probabilidad: ' + str(prediction[0][3]))
     
 
